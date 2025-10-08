91d4ecdb6f3d22412fb6d38e943f429dbf5bf683
ChainsQueen.eth <146336747+ChainsQueenEth@users.noreply.github.com>
2025-10-08 16:07:25 +0100

    feat(types): add Ethereum window object type declarations
    - Define window.ethereum interface
    - Add MetaMask provider types
    - Ensure type safety for Web3 operations

---
+++ src/types/ethereum.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Type declarations for Ethereum provider (MetaMask)
+ */
+
+interface EthereumProvider {
+  isMetaMask?: boolean;
+  request: (args: { method: string; params?: any[] }) => Promise<any>;
+  on: (event: string, callback: (...args: any[]) => void) => void;
+  removeListener: (event: string, callback: (...args: any[]) => void) => void;
+  selectedAddress: string | null;
+}
+
+interface Window {
+  ethereum?: EthereumProvider;
+}
